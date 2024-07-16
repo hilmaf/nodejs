8145a9694761acfd070dce29a87041abf521ecb0
orlando <glee1470@naver.com>
2024-07-16 22:19:11 +0900

    [javascript] 비구조적 할당

---
+++ javascript/nonstructural_allocation.js
@@ -0,0 +1,18 @@
+var candyMachine = {
+    status : {
+        name: 'node',
+        count: 5,
+    },
+    getCandy: function() {
+        this.status.count--;
+        return this.status.count;
+    }
+};
+
+
+// var getCandy = candyMachine.getCandy;
+// var count = candyMachine.status.count;
+
+/////////////////////////////////////////////////////
+
+const { getCandy, status: { count } } = candyMachine;
